895a8ebb6e229af16d431d97ae6a0f5e889ac85f
Valentine <kerubovalentine1@gmail.com>
2023-04-30 16:41:41 +0300

    render botList or message if no bots available

--- src/Components/BotCollection.js
+++ src/Components/BotCollection.js
@@ -2,11 +2,19 @@ import React, { useState, useEffect } from "react";
 import Bot from "./Bot";
 
 function BotCollection({ bots, onAddToArmy }) {
+  //create array of bot components from bots array
   const botList = bots.map((bot) => (
     <Bot key={bot.id} bot={bot} onAddToArmy={onAddToArmy} />
   ));
 
-  return <div>BotCollection</div>;
+  //render BetCollection component
+  return (
+    <div className="bot-collection">
+      <h2>All boats</h2>
+      {/*render botList or message if no bots are available */}
+      {botList.length > 0 ? botList : <p>No bots available</p>}
+    </div>
+  );
 }
 
 export default BotCollection;
