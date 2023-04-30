35942e15895b656aedc49aa2ff149480f3bda5cd
Valentine <kerubovalentine1@gmail.com>
2023-04-30 08:58:03 +0300

    map list of bots to Bot components

--- src/Components/BotCollection.js
+++ src/Components/BotCollection.js
@@ -1,18 +1,22 @@
 import React, { useState, useEffect } from "react";
+import Bot from "./Bot";
 
 //render collection of bots
-function BotCollection() {
+function BotCollection(onAddToArmy) {
   const [bots, setBots] = useState([]);
 
   //fetch list of bots from JSON file
   useEffect(() => {
     fetch("http://localhost:8000/bots")
-    .then((Response) => Response.json())
-    .then(({bots}) => setBots(bots))
-    .catch((error) => console.error(error));
+      .then((Response) => Response.json())
+      .then(({ bots }) => setBots(bots))
+      .catch((error) => console.error(error));
   }, []);
 
- 
+  //map list of bots to Bot components
+  const botList = bots.map((bot) => (
+    <Bot key={bot.id} bot={bot} onAddToArmy={onAddToArmy} />
+  ));
 
   return <div>Bot</div>;
 }
