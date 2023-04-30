023b91eb0bf184b4e9fd8606edb234db7a9a65e6
Valentine <kerubovalentine1@gmail.com>
2023-04-30 07:11:55 +0300

    declare state variables

--- src/Components/BotCollection.js
+++ src/Components/BotCollection.js
@@ -1,6 +1,8 @@
 import React, {useState, useEffect} from 'react'
 
+//render collection of bots
 function BotCollection() {
+    const [bots, setBots] = useState([]);
   return (
     <div>Bot</div>
   )
