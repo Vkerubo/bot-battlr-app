89c3506d7f87f8a0a2d61bd7308e1c241d9814d3
Valentine <kerubovalentine1@gmail.com>
2023-04-30 07:21:40 +0300

    fetch list of bots

--- src/Components/BotCollection.js
+++ src/Components/BotCollection.js
@@ -1,11 +1,18 @@
-import React, {useState, useEffect} from 'react'
+import React, { useState, useEffect } from "react";
 
 //render collection of bots
 function BotCollection() {
-    const [bots, setBots] = useState([]);
-  return (
-    <div>Bot</div>
-  )
+  const [bots, setBots] = useState([]);
+
+  //fetch list of bots from JSON file
+  useEffect(() => {
+    fetch("http://localhost:8000/bots")
+    .then((Response) => Response.json())
+    .then(({bots}) => setBots(bots))
+    .catch((error) => console.error(error));
+  }, []);
+
+  return <div>Bot</div>;
 }
 
-export default BotCollection
+export default BotCollection;
