7897ded948e4a7893747077ebcd82772b0d3df78
Valentine <kerubovalentine1@gmail.com>
2023-04-30 16:33:05 +0300

    create array of bot components

--- src/Components/BotCollection.js
+++ src/Components/BotCollection.js
@@ -1,6 +1,11 @@
 import React, { useState, useEffect } from "react";
+import Bot from "./Bot";
 
-function BotCollection() {
+function BotCollection({ bots, onAddToArmy }) {
+  const botList = bots.map((bot) => (
+    <Bot key={bot.id} bot={bot} onAddToArmy={onAddToArmy} />
+  ));
+
   return <div>BotCollection</div>;
 }
 
