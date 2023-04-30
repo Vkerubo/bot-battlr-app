56847b6595da92c7e3ccb61b35a018ef1bb036fe
Valentine <kerubovalentine1@gmail.com>
2023-04-30 18:14:28 +0300

    create array of bot components from army array

--- src/Components/YourBotArmy.js
+++ src/Components/YourBotArmy.js
@@ -1,6 +1,18 @@
 import React from "react";
+import Bot from "./Bot";
 
-function YourBotArmy() {
+function YourBotArmy({ army, onRemoveFromArmy, onDischarge }) {
+  //create array of bot components from army array
+  const botList = army.map((bot) => (
+    <Bot
+      key={bot.id}
+      bot={bot}
+      onDischarge={onDischarge}
+      onAddToArmy={onRemoveFromArmy}
+    />
+  ));
+
+  //render YourBotArmy component
   return <div>YourBotArmy</div>;
 }
 
