b86d463142afcb18ddd1025563c75b15c2313324
Valentine <kerubovalentine1@gmail.com>
2023-04-30 16:18:19 +0300

    render Bot component

--- src/Components/Bot.js
+++ src/Components/Bot.js
@@ -6,7 +6,27 @@ function Bot({ bot, onAddToArmy, onDischarge }) {
     onAddToArmy(bot);
   }
 
-  return <div>Bot</div>;
+  //render Bot component
+  return (
+    <div className="bot-card">
+      <h3>{bot.name}</h3>
+      <p>
+        <strong>Class:</strong> {bot.bot_class}
+      </p>
+      <p>
+        <strong>Health:</strong>
+        {bot.health}
+      </p>
+      <p>
+        <strong>Damage:</strong>
+        {bot.damage}
+      </p>
+      <p>
+        <strong>Armor:</strong>
+        {bot.armor}
+      </p>
+    </div>
+  );
 }
 
 export default Bot;
